16894b653817340fbd315f4ce0cdcae290f2fdb1
berkaybasol <berkaybasoll@gmail.com>
2025-02-26 16:40:33 +0300

    Başlıyorum

--- src/main/java/com/berkayb/soundconnect/entity/User.java
+++ src/main/java/com/berkayb/soundconnect/entity/User.java
@@ -1,5 +1,29 @@
 package com.berkayb.soundconnect.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.sound.midi.Instrument;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private String description;
 }
