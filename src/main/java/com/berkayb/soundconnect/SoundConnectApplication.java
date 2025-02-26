4e3e497bc8c1e320467e39a7557211c2d6bf085a
berkaybasol <berkaybasoll@gmail.com>
2025-02-26 16:17:14 +0300

    Başlıyorum.

---
+++ src/main/java/com/berkayb/soundconnect/SoundConnectApplication.java
@@ -0,0 +1,13 @@
+package com.berkayb.soundconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundConnectApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SoundConnectApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/berkayb/soundconnect/SoundConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkayb.soundconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundConnectApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
