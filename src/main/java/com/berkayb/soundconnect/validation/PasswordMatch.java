b5790aebc3ca32ebe70175eeff783443459325e2
berkaybasol <berkaybasoll@gmail.com>
2025-03-13 21:40:59 +0300

    custom validation ogrendin. gun sonu

---
+++ src/main/java/com/berkayb/soundconnect/validation/PasswordMatch.java
@@ -0,0 +1,15 @@
+package com.berkayb.soundconnect.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordMatchValidator.class)
+@Target(ElementType.TYPE) // Class veya Record'lara uygulanabilir
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordMatch {
+	String message() default "Şifreler uyuşmuyor.";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
