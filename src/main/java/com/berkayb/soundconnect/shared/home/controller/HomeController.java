6e0d4d5272e05ba04f7fbdbd9760c47a035b7956
berkaybasol <berkaybasoll@gmail.com>
2025-05-02 13:51:50 +0300

    dockerfile acildi..

---
+++ src/main/java/com/berkayb/soundconnect/shared/home/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.berkayb.soundconnect.shared.home.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public ResponseEntity<String> hello() {
+		return ResponseEntity.ok("SoundConnect API is alive 🔥");
+	}
+}
