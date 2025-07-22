5c720f91c085fb0c94c1068a43076f213446b73d
berkaybasol <berkaybasoll@gmail.com>
2025-07-23 00:58:51 +0300

    Mail entegrasyon ayarlari 01

--- src/main/java/com/berkayb/soundconnect/modules/user/entity/User.java
+++ src/main/java/com/berkayb/soundconnect/modules/user/entity/User.java
@@ -56,6 +56,10 @@ public class User extends BaseEntity {
 	
 	private LocalDateTime emailVerificationExpiry;
 	
+	@Column(nullable = false)
+	private Boolean emailVerified = false;
+	
+	
 	@Builder.Default // NullPointer yemeyek diye bos deger atiyo hashsete
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
